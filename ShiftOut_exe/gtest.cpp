afb15e4b5b51f99a71d8a880cd0ec52b945de0f6
buyn <buyn.tex@gmail.com>
2019-04-04 10:37:57 +0300

    New test prototip is ready

--- ShiftOut_exe/gtest.cpp
+++ ShiftOut_exe/gtest.cpp
@@ -36,34 +36,5 @@ TEST(Spi, command_stack) {/*{{{*/
 	EXPECT_FALSE(sspi.spi_sesion)<<"false from sesion end";
 	}/*}}}*/
 
-TEST(Spi, msg_stack_get_one) {/*{{{*/
-	SlaveSPI sspi(8);
-	sspi.setmsg(8);
-	sspi.testmsg(8);
-	EXPECT_EQ(sspi.staksize(), 0);
-	EXPECT_TRUE(sspi.spi_sesion)<<"turue if sesion starteted";
-	sspi.setmsg(SC_ISMSGWATING);
-	sspi.testmsg(SC_ISMSGWATING);
-	EXPECT_EQ(sspi.back_msg, 0);
-	EXPECT_EQ(sspi.staksize(), 0);
-	sspi.addMSG(10,20);
-	sspi.setmsg(SC_ISMSGWATING);
-	sspi.testmsg(SC_ISMSGWATING);
-	EXPECT_EQ(sspi.back_msg, 2);
-	EXPECT_EQ(sspi.staksize(), 0);
-	EXPECT_TRUE(sspi.spi_sesion)<<"turue if sesion starteted";
-	sspi.setmsg(SC_GETMSGBYCOUNT);
-	sspi.testmsg(SC_GETMSGBYCOUNT);
-	EXPECT_EQ(sspi.back_msg, 20);
-	EXPECT_EQ(sspi.staksize(), 0);
-	sspi.setmsg(0);
-	sspi.testmsg(0);
-	EXPECT_EQ(sspi.back_msg, 10);
-	sspi.setmsg(SC_ISMSGWATING);
-	sspi.testmsg(SC_ISMSGWATING);
-	EXPECT_EQ(sspi.back_msg, 0);
-	sspi.setmsg(ENDOFSESION);
-	sspi.testmsg(ENDOFSESION);
-	EXPECT_FALSE(sspi.spi_sesion)<<"false from sesion end";
-	}/*}}}*/
+
 #endif
